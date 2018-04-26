bfe01268b203c5919752e79615dc2121c7cbbed1
trodevel <trodevel@gmail.com>
2018-04-27 01:03:04 +0200

    added output into file

--- generate_report.pl
+++ generate_report.pl
@@ -14,13 +14,15 @@ use warnings;
 # 1.8 - FB08 - bugfix: category name may contain underscore, colon
 # 1.9 - 18409 - fixed perl warnings
 # 1.10 - 18409 - 1. added calculation of totals 2. added output of table headers
+# 1.11 - 18426 - added output into file
 
-my $VER="1.10";
+my $VER="1.11";
 
 ###############################################
 
 sub print_category
 {
+    my $OUTP            = shift;
 my $categ = shift;
 my $categ_mon_ref = shift;
 my $must_calc_avg = shift;
@@ -29,7 +31,7 @@ my $must_calc_total = shift;
 
 my $sum = 0;
 
-print "$categ;";
+print $OUTP "$categ;";
 
 for( my $i = 0; $i < 12; $i = $i + 1 )
 {
@@ -49,27 +51,28 @@ for( my $i = 0; $i < 12; $i = $i + 1 )
 
     if( ! $must_calc_avg )
     {
-        print "$val;";
+        print $OUTP "$val;";
     }
 }
 
 if( $must_calc_avg )
 {
-    printf "%.1f;", ( $sum / $max_month );
+    printf $OUTP "%.1f;", ( $sum / $max_month );
 }
 
 if( $must_calc_total )
 {
-    printf "$sum;";
+    printf $OUTP "$sum;";
 }
 
-print "\n";
+print $OUTP "\n";
 }
 
 ###############################################
 
 sub print_categories_kern
 {
+    my $OUTP            = shift;
 my $list_uniq_categ_ref = shift;
 my $categ_mon_ref = shift;
 my $must_calc_avg = shift;
@@ -78,7 +81,7 @@ my $must_calc_total = shift;
 
 foreach my $categ ( @{$list_uniq_categ_ref} )
 {
-    print_category( $categ, $categ_mon_ref, $must_calc_avg, $max_month, $must_calc_total );
+    print_category( $OUTP, $categ, $categ_mon_ref, $must_calc_avg, $max_month, $must_calc_total );
 }
 
 }
@@ -86,19 +89,21 @@ foreach my $categ ( @{$list_uniq_categ_ref} )
 
 sub print_categories
 {
+    my $OUTP            = shift;
     my $list_uniq_categ_ref = shift;
     my $categ_mon_ref = shift;
 
-    print_categories_kern( $list_uniq_categ_ref, $categ_mon_ref, 0, 0, 1 );
+    print_categories_kern( $OUTP, $list_uniq_categ_ref, $categ_mon_ref, 0, 0, 1 );
 }
 
 sub print_categories_avg
 {
+    my $OUTP            = shift;
     my $list_uniq_categ_ref = shift;
     my $categ_mon_ref = shift;
     my $max_month = shift;
 
-    print_categories_kern( $list_uniq_categ_ref, $categ_mon_ref, 1, $max_month, 0 );
+    print_categories_kern( $OUTP, $list_uniq_categ_ref, $categ_mon_ref, 1, $max_month, 0 );
 }
 
 ###############################################
@@ -133,47 +138,51 @@ sub update_categ
 
 sub print_header_all_months
 {
-    print "category;1;2;3;4;5;6;7;8;9;10;11;12;total;\n";
+    my $OUTP            = shift;
+    print $OUTP "category;1;2;3;4;5;6;7;8;9;10;11;12;total;\n";
 }
 
 ###############################################
 
 sub print_header_averages
 {
-    print "category;average;\n";
+    my $OUTP            = shift;
+    print $OUTP "category;average;\n";
 }
 
 ###############################################
 sub print_report_all_months
 {
+    my $OUTP            = shift;
     my $title           = shift;
     my $list_categ_ref  = shift;
     my $monthly_exp_ref = shift;
-    print "\n";
-    print "$title\n";
-    print "\n";
+    print $OUTP "\n";
+    print $OUTP "$title\n";
+    print $OUTP "\n";
 
-    print_header_all_months();
+    print_header_all_months( $OUTP );
 
-    print_categories( $list_categ_ref, $monthly_exp_ref );
+    print_categories( $OUTP, $list_categ_ref, $monthly_exp_ref );
 }
 
 ###############################################
 
 sub print_report_avg
 {
+    my $OUTP            = shift;
     my $title           = shift;
     my $list_categ_ref  = shift;
     my $monthly_exp_ref = shift;
     my $num_months      = shift;
 
-    print "\n";
-    printf "%s - %.1f months\n", $title, $num_months;
-    print "\n";
+    print $OUTP "\n";
+    printf $OUTP "%s - %.1f months\n", $title, $num_months;
+    print $OUTP "\n";
 
-    print_header_averages();
+    print_header_averages( $OUTP );
 
-    print_categories_avg( $list_categ_ref, $monthly_exp_ref, $num_months );
+    print_categories_avg( $OUTP, $list_categ_ref, $monthly_exp_ref, $num_months );
 }
 
 ###############################################
@@ -193,16 +202,17 @@ sub create_key
 print "generate_report ver. $VER\n";
 
 my $num_args = $#ARGV + 1;
-if( $num_args != 1 )
+if( $num_args != 2 )
 {
-    print STDERR "\nUsage: generate_report.sh <source_file.csv>\n";
+    print STDERR "\nUsage: generate_report.sh <source_file.csv> <output.csv>\n";
     exit;
 }
 
 my $inp = $ARGV[0];
+my $outp = $ARGV[1];
 shift( @ARGV );
 
-#print STDERR "DEBUG: inp = $inp\n";
+print STDERR "DEBUG: inp = $inp outp=$outp\n";
 
 unless( -e $inp )
 {
@@ -239,8 +249,6 @@ my $lines       = 0;
 my $price_rec   = 0;
 my $max_month   = 0;
 my $max_day     = 0;
-
-#open OUTP, ">$outp";
 
 print "Reading $inp...\n";
 open RN, "<", $inp;
@@ -318,8 +326,6 @@ while( <RN> )
 
 close RN;
 
-#close OUTP;
-
 ##########################
 
 my @list_uniq_categ = sort keys %uniq_categ;
@@ -328,23 +334,29 @@ my @list_uniq_categ_owner = sort keys %uniq_categ_owner;
 
 my $compl_months = ( $max_month - 1 ) + ( $max_day / 30 );      # number of completed months
 
-print "\n";
-print "SUMMARY:\n";
-print "unique categories     : " . $#list_uniq_categ ."\n";
-print "unique categ/subcateg : " . $#list_uniq_categ .", ". $#list_uniq_categ_subcateg ."\n";
-print "unique categ/owner    : " . $#list_uniq_categ .", ". $#list_uniq_categ_owner ."\n";
-printf "months, days          : %d, %d, completed months %.1f\n", $max_month, $max_day, $compl_months;
+my $OUTP;
 
-print "lines read/processed  : $lines, $price_rec\n";
-print "warnings              : $warnings\n";
+open( $OUTP, ">$outp" ) or die "Couldn't open file for writing: $!\n";
 
-print_report_all_months( "monthly",            \@list_uniq_categ,          \@mon_categ );
-print_report_all_months( "monthly (subcateg)", \@list_uniq_categ_subcateg, \@mon_categ_subcateg );
+print $OUTP "\n";
+print $OUTP "SUMMARY:\n";
+print $OUTP "unique categories     : " . $#list_uniq_categ ."\n";
+print $OUTP "unique categ/subcateg : " . $#list_uniq_categ .", ". $#list_uniq_categ_subcateg ."\n";
+print $OUTP "unique categ/owner    : " . $#list_uniq_categ .", ". $#list_uniq_categ_owner ."\n";
+printf $OUTP "months, days          : %d, %d, completed months %.1f\n", $max_month, $max_day, $compl_months;
 
-print_report_avg( "averages",            \@list_uniq_categ,          \@mon_categ,          $compl_months );
-print_report_avg( "averages (subcateg)", \@list_uniq_categ_subcateg, \@mon_categ_subcateg, $compl_months );
-print_report_avg( "averages (owner)",    \@list_uniq_categ_owner,    \@mon_categ_owner,    $compl_months );
+print $OUTP "lines read/processed  : $lines, $price_rec\n";
+print $OUTP "warnings              : $warnings\n";
 
-print "\n";
+print_report_all_months( $OUTP, "monthly",            \@list_uniq_categ,          \@mon_categ );
+print_report_all_months( $OUTP, "monthly (subcateg)", \@list_uniq_categ_subcateg, \@mon_categ_subcateg );
+
+print_report_avg( $OUTP, "averages",            \@list_uniq_categ,          \@mon_categ,          $compl_months );
+print_report_avg( $OUTP, "averages (subcateg)", \@list_uniq_categ_subcateg, \@mon_categ_subcateg, $compl_months );
+print_report_avg( $OUTP, "averages (owner)",    \@list_uniq_categ_owner,    \@mon_categ_owner,    $compl_months );
+
+print $OUTP "\n";
+
+close $OUTP;
 
 ##########################
